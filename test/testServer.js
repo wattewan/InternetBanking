e4dfb348cd2c96174fb5f7e9e7ff57d5fab9d0a9
Ewan Watt <watt.ewan99@gmail.com>
2019-05-17 10:06:07 -0700

    Fixing merge

--- test/testServer.js
+++ test/testServer.js
@@ -13,7 +13,49 @@ chai.use(chaiHttp);
 const uri = "mongodb+srv://wattewan:8030113Bst@bankingapp-zcncj.mongodb.net/test?retryWrites=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
+describe('GET Requests', function() {
+    it('GET /', function(done) {
+        chai.request(server)
+            .get('/')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                done();
+            })
+    });
+    it('GET /register', function(done) {
+        chai.request(server)
+            .get('/register')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                done();
+            })
+    });
+    it('GET /confirm-account', function(done) {
+        chai.request(server)
+            .get('/confirm-account')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                done();
+            })
+    });
+    // it('GET /e_transfer', function(done) {
+    //
+    //         chai.request(server)
+    //         .get('/home/e_transfer/Geoffrey')
+    //         .end(function(err, res) {
+    //             res.should.have.status(200);
+    //             done();
+    //         })
+    //
+    // })
+});
+
 describe('POST Requests', function () {
+
+    after((done) => {
+        done();
+        process.exit();
+    });
 
     it('add a single user on /saveUser POST', function (done) {
         client.connect();
@@ -40,42 +82,3 @@ describe('POST Requests', function () {
             });
     })
 });
-
-describe('GET Requests', function() {
-    it('GET /', function(done) {
-        chai.request(server)
-            .get('/')
-            .end(function(err, res) {
-                res.should.have.status(200);
-                done();
-            })
-    });
-    it('GET /register', function(done) {
-        chai.request(server)
-            .get('/register')
-            .end(function(err, res) {
-                res.should.have.status(200);
-                done();
-            })
-    });
-    it('GET /confirm-account', function(done) {
-        chai.request(server)
-            .get('/confirm-account')
-            .end(function(err, res) {
-                res.should.have.status(200);
-                done();
-            })
-    });
-    it('GET /e_transfer', function(done) {
-
-            chai.request(server)
-            .get('/home/e_transfer/Geoffrey')
-            .end(function(err, res) {
-                res.should.have.status(200);
-                done();
-            })
-
-    })
-});
-
-
